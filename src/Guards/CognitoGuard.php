f885c04be7fa731104937c3fdc22e6260ce097f3
Ole Anti <olemahtte@gmail.com>
2024-01-31 19:32:22 +0100

    Create user model if not present

--- src/Guards/CognitoGuard.php
+++ src/Guards/CognitoGuard.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Traits\Macroable;
 use oleanti\LaravelCognito\CognitoClient;
 use oleanti\LaravelCognito\Exceptions\NotAuthorizedException;
 use oleanti\LaravelCognito\Exceptions\NoLocalUserException;
-
+use oleanti\LaravelCognito\Actions\GetUser;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -73,4 +73,34 @@ class CognitoGuard extends SessionGuard implements StatefulGuard
 
         return false;
     }
+/**
+     * Attempt to authenticate a user using the given credentials.
+     *
+     * @param  array  $credentials
+     * @param  bool  $remember
+     * @return bool
+     */
+    public function attempt(array $credentials = [], $remember = false)
+    {
+        $this->fireAttemptEvent($credentials, $remember);
+
+        //$this->lastAttempted = $user = $this->provider->retrieveByCredentials($credentials);
+        $this->lastAttempted = $user = GetUser::retrieveByCredentials($credentials, $this->provider);
+
+        // If an implementation of UserInterface was returned, we'll ask the provider
+        // to validate the user against the given credentials, and if they are in
+        // fact valid we'll log the users into the application and return true.
+        if ($this->hasValidCredentials($user, $credentials)) {
+            $this->login($user, $remember);
+
+            return true;
+        }
+
+        // If the authentication attempt fails we will fire an event so that the user
+        // may be notified of any suspicious attempts to access their account from
+        // an unrecognized user. A developer may listen to this event as needed.
+        $this->fireFailedEvent($user, $credentials);
+
+        return false;
+    }       
 }
